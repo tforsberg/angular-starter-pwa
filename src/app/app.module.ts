c92473e3d37665317c1e4c04dd70d04ebb0f4ac1
Todd Forsberg <tforsberg@gmail.com>
2019-01-01 12:16:44 -0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -11,7 +13,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './auth.guard';
 import { PostsComponent } from './posts/posts.component';
 import { TopNavigationComponent } from './shared/top-navigation/top-navigation.component';
-import { HttpClientModule } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
 
